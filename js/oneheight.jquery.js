06b7de1c1f1a2348a37e2e11c220f1731bd5fb72
Bogardo <chrisbogaards@gmail.com>
2014-02-05 00:03:24 +0100

    Removed all class dependencies and added children selector option to specify which elements to calculate and alter changed .height() to .outerHeight(true) to fix problems with borders, padding and margin (see info file)

--- js/oneheight.jquery.js
+++ js/oneheight.jquery.js
@@ -1,29 +1,45 @@
 (function($){ 
 
-	$.fn.oneheight = function(){
+	$.fn.oneheight = function(options){
 
-       
+        //default settings
+        var defaults = {
+            children: "*" //
+        };
+
+        //merge the settings
+        var settings = $.extend({}, defaults, options);
+
 		this.each(function(){
-		
-        var children = $('.one-height').children('.one-height-element');
+            var container   = jQuery(this),
+                children    = container.children(settings.children);
 
-        var heights = $(children).map(function() {
+            //create array of all heights
+            var heights = $(children).map(function() {
+                return $(this).outerHeight(true);
+            }).get();
 
-            return $(this).height();
+            //calculate maximum height
+            var maxheight       = Math.max.apply(Math, heights),
+                largestChildNum = heights.indexOf(maxheight)
+                largestChild    = $(children[largestChildNum]);
 
-        }).get();
+            //check for borders (top, bottom)
+            var borderTop       = parseInt(largestChild.css("border-top-width")),
+                borderBottom    = parseInt(largestChild.css("border-bottom-width")),
+                totalBorder     = parseInt(borderTop + borderBottom);
+            if (totalBorder > 0) {
+                maxheight = maxheight - totalBorder;
+            }
 
-        var maxheight =  Math.max.apply( Math, heights );
-        
-        for(i = 0; i < children.length; i++){
-            $(children[i]).css( "height", maxheight );
-           
-        }
-
+            //apply maximum height to all children elements
+            for(i = 0; i < children.length; i++){
+                if (i != largestChildNum) {
+                    $(children[i]).css("height", maxheight);
+                }
+            }
 		});
 
 		return this;
 	};
 })(jQuery);
-
-
